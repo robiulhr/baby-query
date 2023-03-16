c21aba28f0a721a1abed7c0d41c38042c7ee397b
Robiul H <robiul@robiul.robiul.dev>
2023-03-16 12:50:41 +0600

    publishing docs on github pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Baby Query',
   description: 'Write Less, Do More.',
+  base:"/baby-query/",
   head: [
     ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/img/favicon_io/apple-touch-icon.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/img/favicon_io/favicon-32x32.png"}],
